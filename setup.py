fa64f37f661f47ff03a39229f31ac76c761d3572
Michael Coulter <michael.e.coulter@gmail.com>
2023-03-07 16:33:45 -0800

    updates for pip install

--- setup.py
+++ setup.py
@@ -2,19 +2,10 @@
 
 from setuptools import find_packages, setup
 
-INSTALL_REQUIRES = ['numpy', 'numba', 'scipy', 'scikit-learn', 'matplotlib',
-                    'xarray', 'pandas', 'dask', 'tqdm', 'statsmodels', 'patsy']
-TESTS_REQUIRE = ['pytest >= 2.7.1']
 
 setup(
-    name='package_template',
-    version='0.1.0.dev0',
-    license='MIT',
-    description=(''),
-    author='',
-    author_email='',
-    url='',
+    name="hippocampus_content_neurofeedback",
+    version="0.1.0.dev0",
+    license="MIT",
     packages=find_packages(),
-    install_requires=INSTALL_REQUIRES,
-    tests_require=TESTS_REQUIRE,
 )
